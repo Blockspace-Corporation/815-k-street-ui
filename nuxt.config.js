00ac7c3a220becb761582ef444613b53a4735d59
JM Mac <johnmarkmancol@gmail.com>
2024-02-26 23:49:53 +0800

    font family

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,12 +15,16 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'preconnect',  href: 'https://fonts.googleapis.com' },
+      { rel: 'preconnect',  href: 'https://fonts.gstatic.com' },
+      { rel: 'stylesheet',  href: 'https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&display=swap'}
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/style.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
