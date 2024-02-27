f6d508bc110d737a7c2da5b1f4ed7e10aa3d9eee
JM Mac <johnmarkmancol@gmail.com>
2024-02-27 14:05:53 +0800

    header shadow

--- components/Body/ImageBanner.vue
+++ components/Body/ImageBanner.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="img-banner flex p-32 text-white bg-cover bg-center items-center" 
+        class="flex p-32 text-white bg-cover bg-center items-center" 
         :class="{
             'h-screen': fullHeight,
             'h-almost': !fullHeight,
@@ -22,15 +22,3 @@ export default {
     }
 }
 </script>
-
-<style scoped>
-.img-banner:after {
-    content: "";
-    width: 100%;
-    height: 0px;
-    box-shadow: 0px 0px 199px 127px #000000;
-    position: absolute;
-    left: 0px;
-    top: 0px;
-}
-</style>
--- components/header/Header.vue
+++ components/header/Header.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="z-10 bg-transparent flex items-center gap-2 justify-between px-8 py-4 fixed w-full top-0 left-0">
+    <div class="banner z-10 bg-transparent flex items-center gap-2 justify-between px-8 py-4 fixed w-full top-0 left-0">
         <div class="w-4/12">
             <HeaderLogo/>
         </div>
@@ -12,3 +12,16 @@
 export default {
 }
 </script>
+
+<style scoped>
+.banner:after {
+    content: "";
+    width: 100%;
+    height: 0px;
+    box-shadow: 0px 0px 199px 127px #000000;
+    position: absolute;
+    left: 0px;
+    top: 0px;
+    z-index: -1;
+}
+</style>
