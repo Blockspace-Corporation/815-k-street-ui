e17e20e662fcbe0442eccc0b99668267047ab4c4
JM Mac <johnmarkmancol@gmail.com>
2024-02-27 22:59:44 +0800

    simple and delicious food

---
+++ components/Sections/SimpleAndDeliciousFood.vue
@@ -0,0 +1,11 @@
+<template>
+    <BodyImageBanner shadowed bg="banners/simple-delicious-banner.webp" class="flex-col justify-center">
+        <center class="z-10">
+            <p class="text-xl text-primary">Enjoy Great Recipe</p>
+            <h2 class="text-6xl font-bold">Simple and Delicious Food</h2>
+            <p>20% Discount for every 5,000 purchase.</p>
+            <br />
+            <Button>place an order</Button>
+        </center class="z-10">
+    </BodyImageBanner>
+</template>
