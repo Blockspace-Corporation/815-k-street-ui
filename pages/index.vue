72826c81e4075df796d1d3341674183c353d0401
JM Mac <johnmarkmancol@gmail.com>
2024-02-27 21:18:10 +0800

    owl carousel

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,7 @@
 import UpgradeToLatestTaste from '../components/Sections/UpgradeToLatestTaste.vue';
 <template>
   <div>
-    <BodyImageBanner bg="banners/home-banner.webp" fullHeight>
-      <div class="w-3/4">
-        <span>Mega Discount on</span>
-        <br />
-        <br />
-        <SectionTitle>Tasty Bites<br />Healthy Foods</SectionTitle>
-        <br />
-        <p class="text-2xl">10 - 50% OFF</p>
-        <br />
-        <Button to="menu">View Menu</Button>
-      </div>
-    </BodyImageBanner>
+    <SectionsHomeSlider />
     <SectionsKoreanChineseCuisine />
     <SectionsUpgradeToLatestTaste />
     <SectionsWhatMakesOurMenuSpecial />
