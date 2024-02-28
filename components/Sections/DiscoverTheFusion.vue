b09b2e615943cd01c95fa6490b85d798753ac4b1
JM Mac <johnmarkmancol@gmail.com>
2024-02-28 09:05:22 +0800

    made some changes

--- components/Sections/DiscoverTheFusion.vue
+++ components/Sections/DiscoverTheFusion.vue
@@ -1,14 +1,16 @@
 <template>
     <BodyImageBanner shadowed bg="banners/about_section_3.webp">
-      <div class="w-1/2 z-10">
-        <SectionTitle class="text-5xl">Discover the Fusion</SectionTitle>
-        <SectionTitle class="text-5xl">Feast: Korean-Chinese</SectionTitle>
-        <SectionTitle class="text-5xl">Culinary Delights</SectionTitle>
+      <div class="w-2/3 z-10">
+        <SectionTitle>
+          Discover the Fusion <br />
+          Feast: Korean-Chinese<br />
+          Culinary Delights
+        </SectionTitle>
         <br />
         <p class="w-2/3">
           Savor the Symmetry of Flavors from Two Rich Culinary Traditions. Feel free to customize the banner with your restaurant's logo, colors, and any additional details that align with your brand identity.
-            <br />
-            <br />
+          <br />
+          <br />
         </p>
       </div>
     </BodyImageBanner>
