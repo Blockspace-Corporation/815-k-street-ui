c687c38b789af12d7e2f98db4fbf53f2cab0a8d9
JM Mac <johnmarkmancol@gmail.com>
2024-02-28 11:25:22 +0800

    menu section

---
+++ components/Divider.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="relative my-4 h-px w-full bg-stroke-grey">
+        <span class="bullet bg-secondary"></span>
+    </div> 
+</template>
+
+<style scoped>
+.bullet {
+    position: absolute;
+    height: 10px;
+    width: 10px;
+    top: -5px;
+    left: 50%;
+    transform: translateX(-50%) rotate(45deg);
+    outline: 3px solid #f9f9f9;
+    border-radius: 1px;
+}
+</style>
---
+++ components/Sections/Menu.vue
@@ -0,0 +1,16 @@
+<template>
+    <BodyPaperField>
+        <div class="flex flex-col gap-2 items-center justify-center text-grey">
+            <h1 class="text-2xl font-bold">Menu</h1>
+            <p class="text-center">
+                For takeout orders please call the restaurant directly at +63 989 4332 546<br />
+                Delivery available through Grab and Food Panda
+            </p>
+        </div>
+        <br />
+        <br />
+        <BodyMenuTabs />
+        <br />
+        <BodyMenuList />
+    </BodyPaperField>
+</template>
