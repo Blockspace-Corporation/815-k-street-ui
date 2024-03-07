77768590cd571798eace57fb1fb85949582fb20d
JM Mac <johnmarkmancol@gmail.com>
2024-03-07 21:09:19 +0800

    change title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'restaurant',
+    title: '815 K Street',
     htmlAttrs: {
       lang: 'en'
     },
